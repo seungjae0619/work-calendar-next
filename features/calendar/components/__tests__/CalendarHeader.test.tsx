24ef59834e2d2961e180cc080778449ab813d374
Park Seungjae <andrewpark0619@gmail.com>
2026-05-04 20:04:57 +0900

    test: add CalendarHeader unit test

---
+++ features/calendar/components/__tests__/CalendarHeader.test.tsx
@@ -0,0 +1,63 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import CalendarHeader from "../CalendarHeader";
+import FullCalendar from "@fullcalendar/react";
+
+jest.mock("../../hooks/useCalendarLogic", () => ({
+  useDialogEvent: jest.fn(() => ({
+    handleDatePickerSelect: jest.fn(),
+  })),
+}));
+
+jest.mock("../CalendarDatePicker", () => {
+  return function mockCalendarDatePicker() {
+    return <div data-testid="mock-date-picker"></div>;
+  };
+});
+
+describe("CalendarHeader", () => {
+  const mockNavigateMonth = jest.fn();
+  const mockToday = jest.fn();
+
+  const fullCalendarRef = {
+    current: {
+      getApi: () => ({
+        today: mockToday,
+      }),
+    } as unknown as FullCalendar,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("이전/다음 버튼 클릭 시 다음 달로 넘어간다", () => {
+    render(
+      <CalendarHeader
+        calendarRef={fullCalendarRef}
+        displayYear={2026}
+        displayMonth={5}
+        navigateMonth={mockNavigateMonth}
+      />,
+    );
+
+    fireEvent.click(screen.getByText("‹"));
+    expect(mockNavigateMonth).toHaveBeenCalledWith("right");
+
+    fireEvent.click(screen.getByText("›"));
+    expect(mockNavigateMonth).toHaveBeenCalledWith("left");
+  });
+
+  it("오늘 버튼 클릭 시 현재 월로 이동한다", () => {
+    render(
+      <CalendarHeader
+        calendarRef={fullCalendarRef}
+        displayYear={2026}
+        displayMonth={5}
+        navigateMonth={mockNavigateMonth}
+      />,
+    );
+
+    fireEvent.click(screen.getByText("오늘"));
+    expect(mockToday).toHaveBeenCalledTimes(1);
+  });
+});
