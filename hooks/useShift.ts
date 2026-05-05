131e6ab7ed7797d335c00ec9bbc634a908258d6f
Park Seungjae <andrewpark0619@gmail.com>
2026-05-05 23:40:51 +0900

    refactor: usePrefecth 훅 삭제 후 useShift 훅 내부에 구현

--- hooks/useShift.ts
+++ hooks/useShift.ts
@@ -1,6 +1,9 @@
 import { getShift } from "@/lib/api/shift";
 import { Shift } from "@/types/shift";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import useCalendarStore from "@/store/calendar";
+import { getMonthRange } from "@/utils/dateHelpers";
 
 export function useShift(startDate: string, endDate: string) {
   const { data: shift = [] } = useQuery<Shift[]>({
@@ -8,5 +11,39 @@ export function useShift(startDate: string, endDate: string) {
     queryFn: () => getShift(startDate, endDate),
   });
 
+  PrefetchShift();
+
   return shift;
 }
+
+const PrefetchShift = () => {
+  const queryClient = useQueryClient();
+
+  const currentDatevalue = useCalendarStore((state) => state.currentDate);
+
+  useEffect(() => {
+    if (!currentDatevalue) return;
+
+    const currentDate = new Date(currentDatevalue);
+
+    const prevRange = getMonthRange(
+      currentDate.getFullYear(),
+      currentDate.getMonth() - 1,
+    );
+    const nextRange = getMonthRange(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+    );
+    queryClient.prefetchQuery({
+      queryKey: ["shift", prevRange.startDate, prevRange.endDate],
+      queryFn: () => getShift(prevRange.startDate, prevRange.endDate),
+      staleTime: 1000 * 60 * 5,
+    });
+
+    queryClient.prefetchQuery({
+      queryKey: ["shift", nextRange.startDate, nextRange.endDate],
+      queryFn: () => getShift(nextRange.startDate, nextRange.endDate),
+      staleTime: 1000 * 60 * 5,
+    });
+  }, [currentDatevalue, queryClient]);
+};
